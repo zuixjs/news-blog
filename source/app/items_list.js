0cbd1b91674538755a2931de4de52b534e5fd087
Gene <genemars@users.noreply.github.com>
2022-03-02 14:09:37 +0100

    Reduced layout shift.

--- source/app/items_list.js
+++ source/app/items_list.js
@@ -28,13 +28,13 @@ function ItemsList(cp) {
           } else {
             el.setAttribute('self', 'size-1of3 lg-half md-half sm-full');
           }
-          el.setAttribute('class', 'card-wrapper'); // <-- will this work?
+          el.setAttribute('class', 'card-wrapper visible-on-ready');
         } else {
           // "mini" layout for subsequent items
           el = zuix.createComponent('items_list/item_mini', options).container();
           // 4 columns layout
           el.setAttribute('self', 'size-1of4 lg-half md-half sm-full');
-          el.setAttribute('class', 'card-wrapper mini'); // <-- will this work?
+          el.setAttribute('class', 'card-wrapper mini visible-on-ready');
         }
         // center the list on wide screens
         el.setAttribute('layout', 'column stretch-center');
