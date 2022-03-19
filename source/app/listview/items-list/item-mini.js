e76a50d3ef5450a43efd99803e10064526ff1091
Gene <genemars@users.noreply.github.com>
2022-03-19 20:19:41 +0100

    moved listview files to own folder.

---
+++ source/app/listview/items-list/item-mini.js
@@ -0,0 +1,24 @@
+/* global zuix */
+'use strict';
+
+function ListIteMini(cp) {
+  cp.create = onCreate;
+
+  function onCreate() {
+    const item = cp.model();
+    cp.field('container')
+        .css('background-image', 'url(' + item.imagePreviewMini+')')
+        .on('click', function() {
+          cp.trigger('item:click', item);
+        });
+    cp.view('.summary')
+        .on('click', function() {
+          cp.trigger('item:click', item);
+        });
+    cp.field('more').on('click', function() {
+      cp.trigger('item:menu', item);
+    });
+  }
+}
+
+module.exports = ListIteMini;
---
+++ source/app/listview/items-list/item.js
@@ -0,0 +1,19 @@
+/* global zuix */
+'use strict';
+
+function ListItem(cp) {
+  cp.create = onCreate;
+
+  function onCreate() {
+    cp.field('container')
+        .css('background-image', 'url(' + cp.model().imagePreview + ')')
+        .on('click', function() {
+          cp.trigger('item:click', cp.model());
+        });
+    cp.field('more').on('click', function() {
+      cp.trigger('item:menu', cp.model());
+    });
+  }
+}
+
+module.exports = ListItem;
