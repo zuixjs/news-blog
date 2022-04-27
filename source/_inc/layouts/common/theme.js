1221f5e28ede79eb67b0f439167cd9fd2e092052
Gene <genemars@users.noreply.github.com>
2022-04-27 11:13:24 +0200

    Fixes dark theme "flash" when changing page.

--- source/_inc/layouts/common/theme.js
+++ source/_inc/layouts/common/theme.js
@@ -4,14 +4,23 @@ function themeSetup() {
   const toggle = doc.find('.theme-toggle');
   const themeDark = 'theme-dark';
   const storeKey = 'app.theme';
+  const fadeColors = {
+    transition: 'color 300ms ease-in, background-color 300ms ease-in'
+  };
+  setTimeout(function() {
+    doc.css(fadeColors);
+    doc.find('header').css(fadeColors);
+    doc.find('nav').css(fadeColors);
+    doc.find('footer').css(fadeColors);
+  }, 100);
   toggle.on('change', toggleTheme);
   toggleTheme();
   function toggleTheme(ev) {
     const checked = ev ?
-            toggle.checked() :
-            localStorage.getItem(storeKey) === 'true';
+      toggle.checked() :
+      localStorage.getItem(storeKey) === 'true';
     !ev && toggle.checked(checked);
-        checked ? doc.addClass(themeDark) : doc.removeClass(themeDark);
-        localStorage.setItem(storeKey, String(checked));
+    checked ? doc.addClass(themeDark) : doc.removeClass(themeDark);
+    localStorage.setItem(storeKey, String(checked));
   }
 }
