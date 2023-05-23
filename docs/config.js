5bcf75a266a97f84f065da8169ea0d6739c130bf
Gene <genemars@users.noreply.github.com>
2023-05-23 19:43:11 +0200

    - fix wrong library path

--- docs/config.js
+++ docs/config.js
@@ -14,7 +14,7 @@
       "baseUrl": "/news-blog/",
       "resourcePath": "/news-blog/app/",
       "libraryPath": {
-        "@lib": "http://zuixjs.github.io/zkit/lib/1.2/",
+        "@lib": "https://zuixjs.github.io/zkit/lib/1.2/",
         "@hgui": "https://genielabs.github.io/homegenie-web-ui/app/",
         "@cdnjs": "https://cdnjs.cloudflare.com/ajax/libs/"
       }
