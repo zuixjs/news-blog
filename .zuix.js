872bbdd46987f569e232d509e02804627a9804b7
gene <generoso.martello@gmail.com>
2025-05-12 19:11:23 +0200

    Upgraded to *zuix-cli* 0.5.4 and *Eleventy* 3.0.

--- .zuix.js
+++ .zuix.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020-2022 G-Labs. All Rights Reserved.
+ * Copyright 2020-2025 G-Labs. All Rights Reserved.
  *         https://zuixjs.github.io/zuix
  *
  * Licensed under the MIT license. See LICENSE file.
@@ -33,32 +33,32 @@ const contentBuildFolder = path.join(buildFolder, contentFolder)
 const {addPage, wipeContent} = require('./.eleventy-zuix');
 
 function collect(value, previous) {
-  return previous.concat([value]);
+    return previous.concat([value]);
 }
 
 module.exports = (program) => {
-  program
-    .command('build')
-    .alias('b')
-    .description('Build web application')
-    .action(() => {
-      // todo: should check if it's a zuix.js project
-      child_process.execSync('npm run build',{
-        stdio:[0, 1, 2]
-      });
-    });
-  program
-    .command('add')
-    .alias('a')
-    .description('Add new page')
-    .requiredOption('-s, --section <section_name>', 'Page section')
-    .requiredOption('-n, --name <page_name>', 'Page name')
-    .option('-l, --layout <layout_template>', 'Layout template name', 'article')
-    .option('-fm, --front-matter "<field>: <value>"', 'Set a front matter field value', collect, [])
-    .action(addPage);
-  program
-    .command('wipe-content')
-    .alias('wc')
-    .description(`Delete all content in "${contentSourceFolder}" and "${contentBuildFolder}" folders.`)
-    .action(wipeContent);
+    program
+        .command('build')
+        .alias('b')
+        .description('Build web application')
+        .action(() => {
+            // todo: should check if it's a zuix.js project
+            child_process.execSync('npm run build',{
+                stdio:[0, 1, 2]
+            });
+        });
+    program
+        .command('add')
+        .alias('a')
+        .description('Add new page')
+        .requiredOption('-s, --section <section_name>', 'Page section')
+        .requiredOption('-n, --name <page_name>', 'Page name')
+        .option('-l, --layout <layout_template>', 'Layout template name', 'article')
+        .option('-f, --front-matter "<field>: <value>"', 'Set a front matter field value', collect, [])
+        .action(addPage);
+    program
+        .command('wipe-content')
+        .alias('wc')
+        .description(`Delete all content in "${contentSourceFolder}" and "${contentBuildFolder}" folders.`)
+        .action(wipeContent);
 };
--- docs/config.js
+++ docs/config.js
@@ -21,11 +21,4 @@
     },
     "siteMapUrl": "https://zuixjs.github.io/news-blog"
   });
-  // Check that service workers are registered
-  if ('serviceWorker' in navigator) {
-    // Use the window load event to keep the page load performant
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/news-blog/service-worker.js');
-    });
-  }
 })();
